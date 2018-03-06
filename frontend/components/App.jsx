5ac1a274516b69679f2d74f0861e3ff0a215d2a5
Dan Kang <dankang27@gmail.com>
2018-03-06 16:48:51 -0500

    Work on nav

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,11 +3,12 @@ import { Route, Link, HashRouter } from 'react-router-dom';
 
 import SignupFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from './session_form/login_form_container';
+import NavContainer from './nav/nav_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div>
-    <h1>Bench BnB</h1>
+    <NavContainer />
     <AuthRoute exact path="/login" component={LoginFormContainer} />
     <AuthRoute exact path="/signup" component={SignupFormContainer} />
   </div>
