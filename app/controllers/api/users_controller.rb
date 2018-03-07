32d072f7bbb0b202bc8a04ecba1141c61da1353d
Dan Kang <dankang27@gmail.com>
2018-03-06 23:11:48 -0500

    Adjust users table and work on sessino form

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
-
+    @user.image_url ||= 'default.jpg'
     if @user.save
       login(@user)
       render "api/users/show"
@@ -13,6 +13,6 @@ class Api::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:email, :first_name, :last_name, :image_url, :password)
   end
 end
