58e06cf497717152a5443c89ad26060f86cb9b01
Dan Kang <dankang27@gmail.com>
2018-03-07 20:01:47 -0500

    Get the nav drop down working

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -3,6 +3,34 @@ import React from 'react';
 export default class Nav extends React.Component {
   constructor(props) {
     super(props);
+    // this.addActiveClass = this.addActiveClass.bind(this);
+    this.state = {
+      active: false
+    };
+    this.toggleClass = this.toggleClass.bind(this);
+  }
+
+  toggleClass() {
+    const currentState = this.state.active;
+    this.setState({ active: !currentState });
+  }
+
+  componentDidMount () {
+    document.addEventListener("click", (e) => {
+      if (!(Object.assign([], e.target.classList).includes("nav-picture")) &&
+          this.state.active === true) {
+        this.toggleClass();
+      }
+    });
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener("click", (e) => {
+      if (!(Object.assign([], e.target.classList).includes("nav-picture")) &&
+          this.state.active === true) {
+        this.toggleClass();
+      }
+    });
   }
 
   signedInNav() {
@@ -13,7 +41,9 @@ export default class Nav extends React.Component {
           <li><a>Saved</a></li>
           <li><a>Trips</a></li>
           <li><a>Help</a></li>
-          <li><a><img className="nav-picture" alt="Profile picture" src="https://a0.muscache.com/im/pictures/41b239ba-1b29-488c-9e84-828d0d5e0073.jpg?aki_policy=profile_small" /></a></li>
+          <li onClick={() => this.toggleClass()}><a><img className="nav-picture" alt="Profile picture" src="https://a0.muscache.com/im/pictures/41b239ba-1b29-488c-9e84-828d0d5e0073.jpg?aki_policy=profile_small" />
+            {this.userDropdown()}
+          </a></li>
         </ul>
       </nav>
     );
@@ -29,6 +59,14 @@ export default class Nav extends React.Component {
           <li onClick={() => this.props.openModal('login')}><a>Log In</a></li>
         </ul>
       </nav>
+    );
+  }
+
+  userDropdown() {
+    return (
+      <ul className={this.state.active ? "nav-user-dropdown-show nav-user-dropdown" : "nav-user-dropdown"}>
+        <li className="nav-user-dropdown-logout" onClick={() => this.props.logout()}>Log Out</li>
+      </ul>
     );
   }
 
