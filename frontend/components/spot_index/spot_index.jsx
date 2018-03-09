8abdbc654d5d7dd0559c5dd733eaf2cd1d9e4dcb
Dan Kang <dankang27@gmail.com>
2018-03-08 23:01:23 -0500

    Tweak colors

--- frontend/components/spot_index/spot_index.jsx
+++ frontend/components/spot_index/spot_index.jsx
@@ -15,11 +15,12 @@ export default class SpotIndex extends React.Component {
     const allSpots = this.props.spots.map((spot) => {
         return <Link to={`/spots/${spot.id}`}><SpotIndexItem key={ spot.id } spot={ spot }></SpotIndexItem></Link>;
     });
+    const spotCount = this.props.spots.length;
     return (
       <main className="spot-index-main">
         <section className="spot-index-left-items">
           <div className="spot-index-header">
-            <h1>275 balmybnb homes in Los Angeles</h1>
+            <h1>{ spotCount } balmybnb homes in Los Angeles</h1>
             <p>Explore homes verified for quality & comfort in cities around the world</p>
           </div>
           <ul>
