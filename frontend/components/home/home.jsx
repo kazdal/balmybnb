5fab1efad3c0d6656b67174b0a20d74d6e13acf2
Dan Kang <dankang27@gmail.com>
2018-03-09 12:14:35 -0500

    Add link to spots on home component

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -9,11 +10,27 @@ export default class Home extends React.Component {
   render () {
 
     return (
-      <section className="home-page">
-        <main>
-          <p>Book unique homes and</p>
-          <p>experiences all over the world.</p>
-        </main>
+      <section className="home-page-container">
+        <section className="home-page">
+          <main>
+            <p>Book unique homes and</p>
+            <p>experiences all over the world.</p>
+          </main>
+        </section>
+
+        <section className="home-explore-section">
+          <h1>Explore balmybnb</h1>
+          <ul>
+            <Link to="/spots">
+              <li>
+                <div className="home-explore-item-picture">IMAGE</div>
+                <div className="home-explore-item-right">
+                  <h3>Homes</h3>
+                </div>
+              </li>
+            </Link>
+          </ul>
+        </section>
       </section>
     );
   }
