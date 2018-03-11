58bbe09217c6de63a92961baf256124cf5229dbc
Dan Kang <dankang27@gmail.com>
2018-03-11 01:02:55 -0500

    Add booking form container

---
+++ frontend/components/spot_show/booking_form_container.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+
+import { createBooking } from '../../actions/booking_actions';
+import BookingForm from './booking_form';
+
+const msp = state => ({
+
+});
+
+const mdp = dispatch => ({
+  createBooking: booking => dispatch(createBooking(booking))
+});
+
+export default connect(msp, mdp)(BookingForm);
