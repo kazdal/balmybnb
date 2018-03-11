b6604f9cd5d179fb457e844a802cbd265eca4683
Dan Kang <dankang27@gmail.com>
2018-03-11 01:05:16 -0500

    Add booking form

---
+++ frontend/components/spot_show/booking_form.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class BookingForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <section>
+        
+      </section>
+    );
+  }
+}
