2ee926dfc038a4c5f93aaafa5b41025d464a5b71
Dan Kang <dankang27@gmail.com>
2018-03-13 21:29:34 -0400

    Integrate rc-slider to correctly filter price

--- frontend/components/filter/price_filter.jsx
+++ frontend/components/filter/price_filter.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Range } from 'rc-slider';
 
 export default class PriceFilter extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      minPrice: 10,
+      maxPrice: 1000
+    };
+    this.updatePrice = this.updatePrice.bind(this);
+  }
+
+  updatePrice(e) {
+    this.setState({
+      minPrice: e[0],
+      maxPrice: e[1]
+    });
+  }
+
+  handleChange(filter, updateFilter) {
+    this.props.updateFilter(filter[0], updateFilter[0]);
+    this.props.updateFilter(filter[1], updateFilter[1]);
+    this.props.closeModal();
   }
 
   render() {
@@ -10,7 +29,17 @@ export default class PriceFilter extends React.Component {
     return (
       <div className="white-modal" onClick={this.props.closeModal}>
         <div className="filter-container" onClick={e => e.stopPropagation()}>
-          THIS A PRICE FILTER
+          <p>
+            ${this.state.minPrice} - ${this.state.maxPrice}
+          </p>
+          <Range
+            min={10}
+            max={1000}
+            allowCross={false}
+            defaultValue={[0, 1000]}
+            onChange={this.updatePrice}/>
+
+          <a onClick={() => this.handleChange(['minPrice', 'maxPrice'], [this.state.minPrice, this.state.maxPrice] )}>Apply</a>
         </div>
       </div>
     );
