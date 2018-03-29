d7011a69ae5dd0c6edb46d22b8a2c1ec87c479b0
Dan Kang <dankang27@gmail.com>
2018-03-29 09:42:58 -0400

    get rid of faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-  require 'faker'
   User.delete_all
   Spot.delete_all
   Booking.delete_all
