11bfc3d858f06ac19227299d17871fe780f3c618
Dan Kang <dankang27@gmail.com>
2018-04-15 20:01:00 -0400

    Fix Linkedin Link

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -28,7 +28,7 @@ export default class Footer extends React.Component {
               </a>
             </li>
             <li>
-              <a href="https://www.linkedin.com/in/dakang">
+              <a href="https://www.linkedin.com/in/dkvng">
                 <i className="fab fa-linkedin-in" />
               </a>
             </li>
